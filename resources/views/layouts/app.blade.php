fe7c5ff15009f7a2e6089746cdd5f0ec1601a2ff
waiphyoaung <waefioong@gmail.com>
2023-11-14 11:48:52 +0630

    Hello World

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    {{-- <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" /> --}}
     <!-- MDB -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.css" rel="stylesheet" />
 
@@ -36,7 +36,7 @@
             @yield('content')
         </div>
         <div class="bottom-menu">
-            <div class="row">
+            <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="d-flex justify-content-between">
                         <a href="">
